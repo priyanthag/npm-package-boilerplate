24617b63bf5bc3cbc3a14a26c063f892092ab2cc
Priyantha Gunawardena <>
2020-03-01 23:13:50 -0600

    adding coverage

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,20 @@ module.exports = {
   transformIgnorePatterns: [
     '<rootDir>/node_modules/(?!lodash-es)'
   ],
-  transform: { '^.+\\.(js|jsx|mjs)$': '<rootDir>/node_modules/babel-jest' }
+  transform: { '^.+\\.(js|jsx|mjs)$': '<rootDir>/node_modules/babel-jest' },
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+    '!**/logs/**',
+    '!**/bin/**',
+    '!*.config.js'
+  ],
+  coverageDirectory: './coverage',
+  coveragePathIgnorePatterns: [
+    './node_modules/',
+    './logs/',
+    './bin/',
+    './coverage/'
+  ]
 }
