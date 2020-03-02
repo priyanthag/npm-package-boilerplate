571d69c4faacbdf894b857227db02140b8abcb11
Priyantha Gunawardena <>
2020-03-01 23:39:21 -0600

    adding babael ignore files

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,9 @@ module.exports = {
   presets: [
     ['@babel/preset-env']
   ],
-  plugins: ['@babel/plugin-transform-destructuring', '@babel/plugin-transform-runtime']
+  plugins: ['@babel/plugin-transform-destructuring', '@babel/plugin-transform-runtime'],
+  ignore: [
+    'src/**/*.test.js',
+    'src/**/__mocks__'
+  ]
 }
